59e10a537e06e0919ad18a979a61fa0928ed491f
Matteo Nardini <matteo.shalen@gmail.com>
2016-04-30 16:12:42 +0200

    DataTable toSMTHStrings and tests
    Added all the significative test cases, corrected various bugs.

--- src/shutils/data/visualization/DataTable.java
+++ src/shutils/data/visualization/DataTable.java
@@ -9,8 +9,6 @@ public class DataTable <T> {
 	
 	int columnNumber;
 	
-	// Data table
-	
 	
 	
 	private DataTable()
@@ -79,13 +77,13 @@ public class DataTable <T> {
 	
 	public int getRowCount()
 	{
-		return getRowCount(false);
+		return getRowCount(true);
 	}
 	
 	public int getRowCount(boolean includeHeadings)
 	{
 		if (includeHeadings)
-			return table.size() + 1;
+			return table.size() + (headings == null ? 0 : 1);
 		else
 			return table.size();
 	}
@@ -100,22 +98,108 @@ public class DataTable <T> {
 	
 	public String toCSVTableString(boolean includeHeadings)
 	{
-		return "";
+		return toCSVTableString(includeHeadings, ";");
+	}
+	
+	public String toCSVTableString(boolean includeHeadings, String separator)
+	{
+		StringBuilder ans = new StringBuilder();
+		
+		if (includeHeadings && headings != null)
+		{
+			ans.append(rowArrayToCSVString(headings, separator));		
+		}
+		
+		for (T[] row : table)
+		{
+			ans.append(rowArrayToCSVString(row, separator));
+		}
+		
+		if(ans.length() > 0)
+			ans.delete(ans.length() - "\n".length(), ans.length());
+		
+		return ans.toString();
 	}
 	
 	public String toLatexTableString(boolean includeHeadings)
 	{
-		return "";
+		StringBuilder ans = new StringBuilder();
+		String CSV = toCSVTableString(includeHeadings, " & ");
+		
+		if (CSV.equals("")) return "";
+		
+		ans.append("\\begin{center}");
+		ans.append("{");
+		
+			ans.append("\\renewcommand{\\arraystretch}{1.5}");
+			ans.append("\\renewcommand{\\tabcolsep}{0.2cm}");
+			
+			ans.append("\\begin{tabular}{|c|c|c|c|}");
+				ans.append("\\hline ");
+				ans.append(CSV.replace("\n", "\\\\ \\hline "));
+				ans.append("\\\\ \\hline ");
+			ans.append("\\end{tabular}");
+		
+		ans.append("}");
+		ans.append("\\end{center}");
+		
+		
+		return ans.toString();
 	}
 	
-	public String toMatlabMatrixString(boolean includeHeadings)
+	public String toMatlabMatrixString(String matrixName)
 	{
-		return "";
+		StringBuilder ans = new StringBuilder();
+		String CSV = toCSVTableString(false, " ");
+		
+		ans.append(matrixName);
+		ans.append(" = [");
+		
+		ans.append(CSV.replace("\n", ";"));
+		
+		ans.append("];");
+		
+		
+		return ans.toString();
 	}
 	
 	public String toHTMLTableString(boolean includeHeadings)
 	{
-		return "";
+		StringBuilder ans = new StringBuilder();
+		String CSV = toCSVTableString(includeHeadings, "</td><td>");
+		
+		if (CSV.equals("")) return "";
+		
+		ans.append("<table>");
+		
+		ans.append("<tr><td>");
+		ans.append(CSV.replace("\n", "</td></tr><tr><td>"));
+		
+		ans.append("</table>");
+		
+		
+		return ans.toString();
+	}
+	
+	
+	private <E> String rowArrayToCSVString(E[] row, String separator)
+	{
+		StringBuilder ans = new StringBuilder();
+		
+		if (row.length == 0) return "";
+		
+		for (E rowElement : row)
+		{
+			ans.append(rowElement.toString());
+			ans.append(separator);
+		}
+		
+		if(ans.subSequence(ans.length() - separator.length(), ans.length()).equals(separator))
+			ans.delete(ans.length() - separator.length(), ans.length());
+		
+		ans.append("\n");
+		
+		return ans.toString();
 	}
 
 }
--- tests/shutils/tests/data/visualization/DataTableTests.java
+++ tests/shutils/tests/data/visualization/DataTableTests.java
@@ -14,7 +14,7 @@ public class DataTableTests {
 
 	/*
 	 * -------------------------------------
-	 * 		GET HEADINGS
+	 * 		getHeadings
 	 * -------------------------------------
 	 */
 	
@@ -65,9 +65,23 @@ public class DataTableTests {
 	
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	/*
 	 * -------------------------------------
-	 * 		SET HEADINGS
+	 * 		setHeadings
 	 * -------------------------------------
 	 */
 	
@@ -105,5 +119,659 @@ public class DataTableTests {
 		t.setHeadings("Test", "Test 2", "Test 3");
 		
 	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#setHeadings()}.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testSetHeadings_02() {
+		
+		DataTable<Integer> t = new DataTable<>(2);
+		
+		t.setHeadings("Test");
+		
+	}
+	
+	
+	
+	
+	
+	/*
+	 * -------------------------------------
+	 * 		addRow
+	 * -------------------------------------
+	 */
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#addRow(Object[])}.
+	 */
+	@Test
+	public void testAddRow_00() {
+		
+		DataTable<Integer> t = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		Integer[] a = {1, 2, 3};
+		t.addRow(a);
+		
+		assertEquals("Test 1;Test 2;Test 3\n1;2;3", t.toString());
+		
+	}
+	
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#addRow(Object[])}.
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void testAddRow_01() {
+		
+		DataTable<Integer> t = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		Integer[] a = {1, 2, 3, 4};
+		t.addRow(a);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * -------------------------------------
+	 * 		removeRowAtIndex
+	 * -------------------------------------
+	 */
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#removeRowAtIndex(Object[])}.
+	 */
+	@Test
+	public void testRemoveRowAtIndex_00() {
+		
+		DataTable<Integer> t = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		Integer[] a = {1, 2, 3};
+		t.addRow(a);
+		t.addRow(a);
+		t.removeRowAtIndex(1);
+		
+		assertEquals("Test 1;Test 2;Test 3\n1;2;3", t.toString());
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#removeRowAtIndex(Object[])}.
+	 */
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testRemoveRowAtIndex_01() {
+		
+		DataTable<Integer> t = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		Integer[] a = {1, 2, 3};
+		t.addRow(a);
+		t.removeRowAtIndex(6);
+	}
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * -------------------------------------
+	 * 		getRowCount
+	 * -------------------------------------
+	 */
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#getRowCount()()}.
+	 */
+	@Test
+	public void testGetRowCount_00() {
+		
+		DataTable<Integer> d = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals(3, d.getRowCount());
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#getRowCount()()}.
+	 */
+	@Test
+	public void testGetRowCount_01() {
+		
+		DataTable<Integer> d = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals(3, d.getRowCount(true));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#getRowCount()()}.
+	 */
+	@Test
+	public void testGetRowCount_02() {
+		
+		DataTable<Integer> d = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals(2, d.getRowCount(false));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#getRowCount()()}.
+	 */
+	@Test
+	public void testGetRowCount_03() {
+		
+		DataTable<Integer> d = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		assertEquals(1, d.getRowCount(true));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#getRowCount()()}.
+	 */
+	@Test
+	public void testGetRowCount_04() {
+		
+		DataTable<Integer> d = new DataTable<>(3);
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals(2, d.getRowCount(true));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#getRowCount()()}.
+	 */
+	@Test
+	public void testGetRowCount_05() {
+		
+		DataTable<Integer> d = new DataTable<>(3);
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals(2, d.getRowCount(false));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#getRowCount()()}.
+	 */
+	@Test
+	public void testGetRowCount_06() {
+		
+		DataTable<Integer> d = new DataTable<>(3);
+		
+		assertEquals(0, d.getRowCount(true));
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * -------------------------------------
+	 * 		toCSVTableString
+	 * -------------------------------------
+	 */
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toCSVTableString}.
+	 */
+	@Test
+	public void testToCSVTableString_00() {
+		
+		DataTable<Integer> d = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals("Test 1;Test 2;Test 3\n1;2;3\n1;2;3", d.toCSVTableString(true, ";"));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toCSVTableString}.
+	 */
+	@Test
+	public void testToCSVTableString_01() {
+		
+		DataTable<Integer> d = new DataTable<>(3);
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals("1;2;3\n1;2;3", d.toCSVTableString(true, ";"));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toCSVTableString}.
+	 */
+	@Test
+	public void testToCSVTableString_02() {
+		
+		DataTable<Integer> d = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals("Test 1 ; Test 2 ; Test 3\n1 ; 2 ; 3\n1 ; 2 ; 3", d.toCSVTableString(true, " ; "));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toCSVTableString}.
+	 */
+	@Test
+	public void testToCSVTableString_03() {
+		
+		DataTable<Integer> d = new DataTable<>(3);
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals("1 ; 2 ; 3\n1 ; 2 ; 3", d.toCSVTableString(true, " ; "));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toCSVTableString}.
+	 */
+	@Test
+	public void testToCSVTableString_04() {
+		
+		DataTable<Integer> d = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals("1;2;3\n1;2;3", d.toCSVTableString(false, ";"));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toCSVTableString}.
+	 */
+	@Test
+	public void testToCSVTableString_05() {
+		
+		DataTable<Integer> d = new DataTable<>(3);
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals("1;2;3\n1;2;3", d.toCSVTableString(false, ";"));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toCSVTableString}.
+	 */
+	@Test
+	public void testToCSVTableString_06() {
+		
+		DataTable<Integer> d = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals("1 ; 2 ; 3\n1 ; 2 ; 3", d.toCSVTableString(false, " ; "));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toCSVTableString}.
+	 */
+	@Test
+	public void testToCSVTableString_07() {
+		
+		DataTable<Integer> d = new DataTable<>(3);
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals("1 ; 2 ; 3\n1 ; 2 ; 3", d.toCSVTableString(false, " ; "));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toCSVTableString}.
+	 */
+	@Test
+	public void testToCSVTableString_08() {
+		
+		DataTable<Integer> d = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		assertEquals("Test 1;Test 2;Test 3", d.toCSVTableString(true, ";"));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toCSVTableString}.
+	 */
+	@Test
+	public void testToCSVTableString_09() {
+		
+		DataTable<Integer> d = new DataTable<>(3);
+		
+		assertEquals("", d.toCSVTableString(true, ";"));
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * -------------------------------------
+	 * 		toLatexTableString
+	 * -------------------------------------
+	 */
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toLatexTableString}.
+	 */
+	@Test
+	public void testToLatexTableString_00() {
+		
+		DataTable<Integer> d = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals("\\begin{center}{\\renewcommand{\\arraystretch}{1.5}\\renewcommand{\\tabcolsep}"
+				+ "{0.2cm}\\begin{tabular}{|c|c|c|c|}\\hline Test 1 & Test 2 & Test 3\\\\ \\hline 1 & 2 & 3"
+				+ "\\\\ \\hline 1 & 2 & 3\\\\ \\hline \\end{tabular}}\\end{center}", d.toLatexTableString(true));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toLatexTableString}.
+	 */
+	@Test
+	public void testToLatexTableString_01() {
+		
+		DataTable<Integer> d = new DataTable<>(3);
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals("\\begin{center}{\\renewcommand{\\arraystretch}{1.5}\\renewcommand{\\tabcolsep}"
+				+ "{0.2cm}\\begin{tabular}{|c|c|c|c|}\\hline 1 & 2 & 3\\\\ \\hline 1 & 2 & 3\\\\ \\hline "
+				+ "\\end{tabular}}\\end{center}", d.toLatexTableString(true));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toLatexTableString}.
+	 */
+	@Test
+	public void testToLatexTableString_02() {
+		
+		DataTable<Integer> d = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals("\\begin{center}{\\renewcommand{\\arraystretch}{1.5}\\renewcommand{\\tabcolsep}"
+				+ "{0.2cm}\\begin{tabular}{|c|c|c|c|}\\hline 1 & 2 & 3\\\\ \\hline 1 & 2 & 3\\\\ \\hline "
+				+ "\\end{tabular}}\\end{center}", d.toLatexTableString(false));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toLatexTableString}.
+	 */
+	@Test
+	public void testToLatexTableString_03() {
+		
+		DataTable<Integer> d = new DataTable<>(3);
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals("\\begin{center}{\\renewcommand{\\arraystretch}{1.5}\\renewcommand{\\tabcolsep}"
+				+ "{0.2cm}\\begin{tabular}{|c|c|c|c|}\\hline 1 & 2 & 3\\\\ \\hline 1 & 2 & 3\\\\ \\hline "
+				+ "\\end{tabular}}\\end{center}", d.toLatexTableString(false));
+		
+	}
+	
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toLatexTableString}.
+	 */
+	@Test
+	public void testToLatexTableString_04() {
+		
+		DataTable<Integer> d = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		assertEquals("\\begin{center}{\\renewcommand{\\arraystretch}{1.5}"
+				+ "\\renewcommand{\\tabcolsep}{0.2cm}\\begin{tabular}{|c|c|c|c|}\\hline Test "
+				+ "1 & Test 2 & Test 3\\\\ \\hline \\end{tabular}}\\end{center}", d.toLatexTableString(true));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toLatexTableString}.
+	 */
+	@Test
+	public void testToLatexTableString_05() {
+		
+		DataTable<Integer> d = new DataTable<>(3);
+		
+		assertEquals("", d.toLatexTableString(true));
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * -------------------------------------
+	 * 		toMatlabMatrixString
+	 * -------------------------------------
+	 */
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toMatlabMatrixString}.
+	 */
+	@Test
+	public void testToMatlabMatrixString_00() {
+		
+		DataTable<Integer> d = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals("Test = [1 2 3;1 2 3];", d.toMatlabMatrixString("Test"));
+		
+	}
+	
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toMatlabMatrixString}.
+	 */
+	@Test
+	public void testToMatlabMatrixString_01() {
+		
+		DataTable<Integer> d = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		assertEquals("Test = [];", d.toMatlabMatrixString("Test"));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toMatlabMatrixString}.
+	 */
+	@Test
+	public void testToMatlabMatrixString_02() {
+		
+		DataTable<Integer> d = new DataTable<>(3);
+		
+		assertEquals("Test = [];", d.toMatlabMatrixString("Test"));
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * -------------------------------------
+	 * 		toHTMLTableString
+	 * -------------------------------------
+	 */
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toHTMLTableString}.
+	 */
+	@Test
+	public void testToHTMLTableString_00() {
+		
+		DataTable<Integer> d = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals("<table><tr><td>Test 1</td><td>Test 2</td><td>Test 3</td></tr><tr><td>1</td><td>2</td>"
+				+ "<td>3</td></tr><tr><td>1</td><td>2</td><td>3</table>", d.toHTMLTableString(true));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toHTMLTableString}.
+	 */
+	@Test
+	public void testToHTMLTableString_01() {
+		
+		DataTable<Integer> d = new DataTable<>(3);
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals("<table><tr><td>1</td><td>2</td><td>3</td></tr>"
+				+ "<tr><td>1</td><td>2</td><td>3</table>", d.toHTMLTableString(true));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toHTMLTableString}.
+	 */
+	@Test
+	public void testToHTMLTableString_02() {
+		
+		DataTable<Integer> d = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals("<table><tr><td>1</td><td>2</td><td>3</td></tr>"
+				+ "<tr><td>1</td><td>2</td><td>3</table>", d.toHTMLTableString(false));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toHTMLTableString}.
+	 */
+	@Test
+	public void testToHTMLTableString_03() {
+		
+		DataTable<Integer> d = new DataTable<>(3);
+		
+		Integer[] a = {1, 2, 3};
+		d.addRow(a);
+		d.addRow(a);
+		
+		assertEquals("<table><tr><td>1</td><td>2</td><td>3</td></tr>"
+				+ "<tr><td>1</td><td>2</td><td>3</table>", d.toHTMLTableString(false));
+		
+	}
+	
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toHTMLTableString}.
+	 */
+	@Test
+	public void testToHTMLTableString_04() {
+		
+		DataTable<Integer> d = new DataTable<>("Test 1", "Test 2", "Test 3");
+		
+		assertEquals("<table><tr><td>Test 1</td><td>Test 2</td><td>Test 3</table>", d.toHTMLTableString(true));
+		
+	}
+	
+	/**
+	 * Test method for {@link shutils.data.visualization.DataTable#toHTMLTableString}.
+	 */
+	@Test
+	public void testToHTMLTableString_05() {
+		
+		DataTable<Integer> d = new DataTable<>(3);
+		
+		assertEquals("", d.toHTMLTableString(true));
+		
+	}
+	
 
 }
