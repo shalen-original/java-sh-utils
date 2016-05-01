1bdc537d06a7e202b8f239d988c07fcce28b23b5
Matteo Nardini <matteo.shalen@gmail.com>
2016-05-01 11:44:19 +0200

    findMinInRange, findMin: SHArray rewriting and testing + checkEndpoints
    And doc correction

--- src/shutils/array/SHArray.java
+++ src/shutils/array/SHArray.java
@@ -6,6 +6,12 @@ package shutils.array;
  */
 public class SHArray {
 
+	
+	/*
+	 *  FIND MAX
+	 */
+	
+	
 	/**
 	 * Finds the max element of the array in the interval {@code (i;j)}.
 	 * @param A The array which max has to be found.
@@ -20,10 +26,7 @@ public class SHArray {
 	{
 		if(A.length == 0) return null;
 		
-		if (i > j) throw new IllegalArgumentException("The lower endpoint is greater than the upper one.");
-		
-		if (i < 0 || j >= A.length)
-			throw new IndexOutOfBoundsException("The endpoints are not valid.");
+		checkEndpoints(A.length, i, j);
 		
 		T max = A[i];
 		for(;i <= j; i++)
@@ -39,8 +42,6 @@ public class SHArray {
 	 * @param A The array which max has to be found.
 	 * @param <T> The type of elements contained in {@code A}.
 	 * @return The max element of the array {@code A}.
-	 * @throws IllegalArgumentException When {@code i > j}.
-	 * @throws IndexOutOfBoundsException When {@code i < 0} or {@code j >= A.length}.
 	 */
 	public static <T extends Comparable<T>> T findMax(T[] A)
 	{
@@ -62,10 +63,7 @@ public class SHArray {
 	{
 		if(A.length == 0) return null;
 		
-		if (i > j) throw new IllegalArgumentException("The lower endpoint is greater than the upper one.");
-		
-		if (i < 0 || j >= A.length)
-			throw new IndexOutOfBoundsException("The endpoints are not valid.");
+		checkEndpoints(A.length, i, j);
 		
 		int maxPos = i;
 		
@@ -84,13 +82,131 @@ public class SHArray {
 	 * Finds the max element position of the array.
 	 * @param A The array which max position has to be found.
 	 * @param <T> The type of elements contained in {@code A}.
-	 * @return The max element position of the array {@code A}.
-	 * @throws IllegalArgumentException When {@code i > j}.
-	 * @throws IndexOutOfBoundsException When {@code i < 0} or {@code j >= A.length}.
+	 * @return The max element position of the array {@code A}.	
 	 */
 	public static <T extends Comparable<T>> Integer findMaxPos(T[] A)
 	{
 		return findMaxPosInRange(A, 0, A.length - 1);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 *  FIND MIN
+	 */
+	
+	/**
+	 * Finds the min element of the array in the interval {@code (i;j)}.
+	 * @param A The array which min has to be found.
+	 * @param i The lower endpoint of the search interval (inclusive).
+	 * @param j The upper endpoint of the search interval (inclusive).
+	 * @return The min element of the array {@code A} in the interval {@code (i;j)}.
+	 * @throws IllegalArgumentException When {@code i > j}.
+	 * @throws IndexOutOfBoundsException When {@code i < 0} or {@code j >= A.length}.
+	 */
+	public static <T extends Comparable<T>> T findMinInRange(T[] A, int i, int j)
+	{
+		if(A.length == 0) return null;
+		
+		if (i > j) throw new IllegalArgumentException("The lower endpoint is greater than the upper one.");
+		
+		checkEndpoints(A.length, i, j);
+		
+		T min = A[i];
+		for(;i <= j; i++)
+		{
+			if(A[i].compareTo(min) < 0) min = A[i];
+		}
+		
+		return min;
+	}
+	
+	
+	/**
+	 * Finds the min element of the array.
+	 * @param A The array which min has to be found.
+	 * @param <T> The type of elements contained in {@code A}.
+	 * @return The min element of the array {@code A}.
+	 */
+	public static <T extends Comparable<T>> T findMin(T[] A)
+	{
+		return findMinInRange(A, 0, A.length - 1);
+	}
+	
+	
+	/**
+	 * Finds the min element position of the array in the interval {@code (i;j)}.
+	 * @param A The array which min position has to be found.
+	 * @param i The lower endpoint of the search interval (inclusive).
+	 * @param j The upper endpoint of the search interval (inclusive).
+	 * @param <T> The type of elements contained in {@code A}.
+	 * @return The min element position of the array {@code A} in the interval {@code (i;j)}.
+	 * @throws IllegalArgumentException When {@code i > j}.
+	 * @throws IndexOutOfBoundsException When {@code i < 0} or {@code j >= A.length}.
+	 */
+	public static <T extends Comparable<T>> Integer findMinPosInRange(T[] A, int i, int j)
+	{
+		if(A.length == 0) return null;
+		
+		checkEndpoints(A.length, i, j);
+		
+		int minPos = i;
+		
+		for (; i <= j; i++)
+		{
+			if (A[i].compareTo(A[minPos]) < 0)
+			{
+				minPos = i;
+			}
+		}
+		
+		return minPos;
+	}
+
+	/**
+	 * Finds the min element position of the array.
+	 * @param A The array which min position has to be found.
+	 * @param <T> The type of elements contained in {@code A}.
+	 * @return The min element position of the array {@code A}.	
+	 */
+	public static <T extends Comparable<T>> Integer findMinPos(T[] A)
+	{
+		return findMinPosInRange(A, 0, A.length - 1);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * 	INTERNAL UTILITIES 
+	 */
+
+	/**
+	 * Internal utility to check if the endpoints are valid.
+	 * @param l The length of the array.
+	 * @param i The left (lower) endpoint.
+	 * @param j The right (greater) endpoint. 
+	 * @throws IllegalArgumentException When {@code i > j}.
+	 * @throws IndexOutOfBoundsException When {@code i < 0} or {@code j >= A.length}.
+	 */
+	private static void checkEndpoints(int l, int i, int j)
+	{		
+		if (i > j) throw new IllegalArgumentException("The lower endpoint is greater than the upper one.");
+		
+		if (i < 0 || j >= l)
+			throw new IndexOutOfBoundsException("The endpoints are not valid.");
 	}
 	
 }
--- tests/shutils/tests/array/SHArrayTests.java
+++ tests/shutils/tests/array/SHArrayTests.java
@@ -8,6 +8,8 @@ import shutils.array.SHArray;
 
 public class SHArrayTests {
 
+	// FIND MAX
+	
 	/*
 	 *	findMaxInRange 
 	 */
@@ -162,7 +164,7 @@ public class SHArrayTests {
 
 	
 	/*
-	 *	findMax
+	 *	findMaxPos
 	 */
 	
 	@Test
@@ -173,4 +175,173 @@ public class SHArrayTests {
 		assertEquals(4, (int)SHArray.findMaxPos(A));
 	}
 
+	
+	
+	// FIND MIN
+	/*
+	 *	findMinInRange 
+	 */
+	
+	@Test
+	public void testFindMinInRange_00() {
+		
+		Integer[] A = {2, 5, 4, 7, 8, 7, 2};
+		
+		assertEquals(2, (int)SHArray.findMinInRange(A, 0, A.length - 1));
+	}
+	
+	@Test
+	public void testFindMinInRange_01() {
+		
+		Integer[] A = {2, 5, 4, 7, 8, 7, 2};
+		
+		assertEquals(4, (int)SHArray.findMinInRange(A, 1, 3));
+	}
+	
+	@Test
+	public void testFindMinInRange_02() {
+		
+		Integer[] A = {2, 5, 4, 7, 8, 7, 2};
+		
+		assertEquals(5, (int)SHArray.findMinInRange(A, 1, 1));
+	}
+	
+	@Test
+	public void testFindMinInRange_03() {
+		
+		Integer[] A = {};
+		
+		assertEquals(null, SHArray.findMinInRange(A, 0, 0));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testFindMinInRange_04() {
+		
+		Integer[] A = {3, 6, 5, 8};
+		
+		SHArray.findMinInRange(A, 6, 2);
+	}
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void testFindMinInRange_05() {
+		
+		Integer[] A = {3, 6, 5, 8};
+		
+		SHArray.findMinInRange(A, -1, 2);
+	}
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void testFindMinInRange_06() {
+		
+		Integer[] A = {3, 6, 5, 8};
+		
+		SHArray.findMinInRange(A, 5, 6);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testFindMinInRange_07() {
+		
+		Integer[] A = {3, 6, 5, 8};
+		
+		SHArray.findMinInRange(A, 1, -1);
+	}
+	
+
+	
+	/*
+	 *	findMin
+	 */
+	
+	@Test
+	public void testFindMin_00() {
+		
+		Integer[] A = {2, 5, 4, 7, 8, 7, 2};
+		
+		assertEquals(2, (int)SHArray.findMin(A));
+	}
+
+	
+	/*
+	 *	findMinPosInRange 
+	 */
+	
+	@Test
+	public void testFindMinPosInRange_00() {
+		
+		Integer[] A = {2, 5, 4, 7, 8, 7, 2};
+		
+		assertEquals(0, (int)SHArray.findMinPosInRange(A, 0, A.length - 1));
+	}
+	
+	@Test
+	public void testFindMinPosInRange_01() {
+		
+		Integer[] A = {2, 5, 4, 7, 8, 7, 2};
+		
+		assertEquals(2, (int)SHArray.findMinPosInRange(A, 1, 3));
+	}
+	
+	@Test
+	public void testFindMinPosInRange_02() {
+		
+		Integer[] A = {2, 5, 4, 7, 8, 7, 2};
+		
+		assertEquals(1, (int)SHArray.findMinPosInRange(A, 1, 1));
+	}
+	
+	@Test
+	public void testFindMinPosInRange_03() {
+		
+		Integer[] A = {};
+		
+		assertEquals(null, SHArray.findMinPosInRange(A, 0, 0));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testFindMinPosInRange_04() {
+		
+		Integer[] A = {3, 6, 5, 8};
+		
+		SHArray.findMinPosInRange(A, 6, 2);
+	}
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void testFindMinPosInRange_05() {
+		
+		Integer[] A = {3, 6, 5, 8};
+		
+		SHArray.findMinPosInRange(A, -1, 2);
+	}
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void testFindMinPosInRange_06() {
+		
+		Integer[] A = {3, 6, 5, 8};
+		
+		SHArray.findMinPosInRange(A, 5, 6);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testFindMinPosInRange_07() {
+		
+		Integer[] A = {3, 6, 5, 8};
+		
+		SHArray.findMinPosInRange(A, 1, -1);
+	}
+	
+
+	
+	/*
+	 *	findMinPos
+	 */
+	
+	@Test
+	public void testFindMinPos_00() {
+		
+		Integer[] A = {2, 5, 4, 7, 8, 7, 2};
+		
+		assertEquals(0, (int)SHArray.findMinPos(A));
+	}
+	
+	
 }
