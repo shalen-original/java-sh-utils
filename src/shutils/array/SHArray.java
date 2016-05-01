888a8ec6c0659184e7a8fc6c6af23e6fc09350ae
Matteo Nardini <matteo.shalen@gmail.com>
2016-05-01 11:26:52 +0200

    findMaxPosInRange, findMaxPos: SHArray rewriting an testing

--- src/shutils/array/SHArray.java
+++ src/shutils/array/SHArray.java
@@ -48,6 +48,49 @@ public class SHArray {
 	}
 	
 	
+	/**
+	 * Finds the max element position of the array in the interval {@code (i;j)}.
+	 * @param A The array which max position has to be found.
+	 * @param i The lower endpoint of the search interval (inclusive).
+	 * @param j The upper endpoint of the search interval (inclusive).
+	 * @param <T> The type of elements contained in {@code A}.
+	 * @return The max element position of the array {@code A} in the interval {@code (i;j)}.
+	 * @throws IllegalArgumentException When {@code i > j}.
+	 * @throws IndexOutOfBoundsException When {@code i < 0} or {@code j >= A.length}.
+	 */
+	public static <T extends Comparable<T>> Integer findMaxPosInRange(T[] A, int i, int j)
+	{
+		if(A.length == 0) return null;
+		
+		if (i > j) throw new IllegalArgumentException("The lower endpoint is greater than the upper one.");
+		
+		if (i < 0 || j >= A.length)
+			throw new IndexOutOfBoundsException("The endpoints are not valid.");
+		
+		int maxPos = i;
+		
+		for (; i <= j; i++)
+		{
+			if (A[i].compareTo(A[maxPos]) > 0)
+			{
+				maxPos = i;
+			}
+		}
+		
+		return maxPos;
+	}
 
+	/**
+	 * Finds the max element position of the array.
+	 * @param A The array which max position has to be found.
+	 * @param <T> The type of elements contained in {@code A}.
+	 * @return The max element position of the array {@code A}.
+	 * @throws IllegalArgumentException When {@code i > j}.
+	 * @throws IndexOutOfBoundsException When {@code i < 0} or {@code j >= A.length}.
+	 */
+	public static <T extends Comparable<T>> Integer findMaxPos(T[] A)
+	{
+		return findMaxPosInRange(A, 0, A.length - 1);
+	}
 	
 }
--- tests/shutils/tests/array/SHArrayTests.java
+++ tests/shutils/tests/array/SHArrayTests.java
@@ -49,7 +49,7 @@ public class SHArrayTests {
 		
 		Integer[] A = {3, 6, 5, 8};
 		
-		assertEquals(null, SHArray.findMaxInRange(A, 6, 2));
+		SHArray.findMaxInRange(A, 6, 2);
 	}
 	
 	@Test(expected=IndexOutOfBoundsException.class)
@@ -57,7 +57,7 @@ public class SHArrayTests {
 		
 		Integer[] A = {3, 6, 5, 8};
 		
-		assertEquals(null, SHArray.findMaxInRange(A, -1, 2));
+		SHArray.findMaxInRange(A, -1, 2);
 	}
 	
 	@Test(expected=IndexOutOfBoundsException.class)
@@ -65,7 +65,7 @@ public class SHArrayTests {
 		
 		Integer[] A = {3, 6, 5, 8};
 		
-		assertEquals(null, SHArray.findMaxInRange(A, 5, 6));
+		SHArray.findMaxInRange(A, 5, 6);
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
@@ -73,7 +73,7 @@ public class SHArrayTests {
 		
 		Integer[] A = {3, 6, 5, 8};
 		
-		assertEquals(null, SHArray.findMaxInRange(A, 1, -1));
+		SHArray.findMaxInRange(A, 1, -1);
 	}
 	
 
@@ -90,5 +90,87 @@ public class SHArrayTests {
 		assertEquals(8, (int)SHArray.findMax(A));
 	}
 
+	
+	/*
+	 *	findMaxPosInRange 
+	 */
+	
+	@Test
+	public void testFindMaxPosInRange_00() {
+		
+		Integer[] A = {2, 5, 4, 7, 8, 7, 2};
+		
+		assertEquals(4, (int)SHArray.findMaxPosInRange(A, 0, A.length - 1));
+	}
+	
+	@Test
+	public void testFindMaxPosInRange_01() {
+		
+		Integer[] A = {2, 5, 4, 7, 8, 7, 2};
+		
+		assertEquals(3, (int)SHArray.findMaxPosInRange(A, 1, 3));
+	}
+	
+	@Test
+	public void testFindMaxPosInRange_02() {
+		
+		Integer[] A = {2, 5, 4, 7, 8, 7, 2};
+		
+		assertEquals(1, (int)SHArray.findMaxPosInRange(A, 1, 1));
+	}
+	
+	@Test
+	public void testFindMaxPosInRange_03() {
+		
+		Integer[] A = {};
+		
+		assertEquals(null, SHArray.findMaxPosInRange(A, 0, 0));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testFindMaxPosInRange_04() {
+		
+		Integer[] A = {3, 6, 5, 8};
+		
+		SHArray.findMaxPosInRange(A, 6, 2);
+	}
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void testFindMaxPosInRange_05() {
+		
+		Integer[] A = {3, 6, 5, 8};
+		
+		SHArray.findMaxPosInRange(A, -1, 2);
+	}
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void testFindMaxPosInRange_06() {
+		
+		Integer[] A = {3, 6, 5, 8};
+		
+		SHArray.findMaxPosInRange(A, 5, 6);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testFindMaxPosInRange_07() {
+		
+		Integer[] A = {3, 6, 5, 8};
+		
+		SHArray.findMaxPosInRange(A, 1, -1);
+	}
+	
+
+	
+	/*
+	 *	findMax
+	 */
+	
+	@Test
+	public void testFindMaxPos_00() {
+		
+		Integer[] A = {2, 5, 4, 7, 8, 7, 2};
+		
+		assertEquals(4, (int)SHArray.findMaxPos(A));
+	}
 
 }
